f840f79ac97959d2504020d5c173457ef12f0b11
Muhammad Rizky Firdaus <167732712+dausrizky109@users.noreply.github.com>
2024-07-21 17:36:27 +0700

    Google Analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,6 +213,14 @@
 
     <!-- Template Javascript -->
     <script src="{{asset('assets/js/main.js')}}"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H5PRL106R3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H5PRL106R3');
+    </script>
     <script>
         document.addEventListener("DOMContentLoaded", function () {
             const loader = document.getElementById('loader');
