0c0bf09a239b495629007b3039410357728b5f1b
Muhammad Rizky Firdaus <167732712+dausrizky109@users.noreply.github.com>
2024-10-15 03:04:39 +0700

    update compro file

--- resources/views/sections/about-services.blade.php
+++ resources/views/sections/about-services.blade.php
@@ -25,7 +25,7 @@
                 </p>
                 <button data-bs-toggle="modal" data-bs-target="#seeMore"
                     class="btn btn-primary-custom pt-2 px-5 me-3 animated slideInRight mb-3">@lang('landing.about_us.see_more')</button>
-                @if ($compro->value)
+                @if ($compro && $compro->value)
                     <button data-bs-toggle="modal" data-bs-target="#modalFileSend"
                         class="btn btn-primary-custom pt-2 px-5 me-3 animated slideInRight mb-3">
                         <svg width="30" height="20" viewBox="0 2 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
